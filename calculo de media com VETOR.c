269e626924c9236a932ddde908091515e373fecb
RHAN4 <159554135+RHAN4@users.noreply.github.com>
2024-04-09 20:12:24 -0300

    Add files via upload

---
+++ calculo de media com VETOR.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <ctype.h>
+#include <limits.h>
+#include <string.h>
+
+void cabecalho () {
+  printf("\n== SENAI ==\n");
+}
+
+int main() {
+  cabecalho();
+  setlocale(LC_ALL, "");
+  
+  int nota [3], soma = 0, contador = 0, media = 0;
+
+  for(int i = 0; i < 3; i++) {
+    printf("Digite %dº nota: ", i + 1);
+    scanf("%d",&nota[i]);
+
+    soma += nota[i];
+    contador++;
+  }
+
+  printf("\nNotas informadas: \n");
+  for(int i = 0; i < 3; i++) {
+    printf("%dº nota: %d\n", i + 1, nota[i]);
+  }
+
+  media = soma / contador;
+
+  printf("Média: %i", media);
+  
+  return 0;
+}
